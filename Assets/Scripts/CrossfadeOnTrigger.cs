e59768f8cf07ca6504e8001ade5ddd52f54ecdb5
Jesse Riggins <jesse.riggins98@gmail.com>
2019-09-20 13:39:10 -0400

    Adds script CrossfadeOnTrigger.

---
+++ Assets/Scripts/CrossfadeOnTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrossfadeOnTrigger : MonoBehaviour
+{
+	public AudioClip[] tracks;
+
+	public float fadeTime = 1.0f;
+
+	public int currentTrack = 0;
+
+    public bool triggerMusic = false;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(triggerMusic)
+		{
+			if(currentTrack >= tracks.Length)
+			{
+				currentTrack = 0;
+			}
+			MusicManager.Crossfade(tracks[currentTrack], fadeTime);
+
+            triggerMusic = false;
+		}
+	}
+}
