04f3f6325beaced1cbb92fc55eb3a09ef92f356b
GalacticGardener <esrausch@umail.iu.edu>
2019-10-15 00:18:29 -0400

    Got the line renderer partially working. It will connect to one box and then, once I start moving, remain the same length as on start and then continuously point in that box's direction. Steps in the right direction, but still need to fix it so that it will only appear when I collide with a box and then appear between each box.

--- Assets/Art/VFX Prefabs/Inventory_FX/Inventory_Rope.cs
+++ Assets/Art/VFX Prefabs/Inventory_FX/Inventory_Rope.cs
@@ -5,18 +5,40 @@ using UnityEngine;
 public class Inventory_Rope : MonoBehaviour
 {
     private LineRenderer lineRenderer;
+    private float counter;
+    private float dist;
 
     public Transform player;
+    public Transform destination;
+
+    public float lineDrawSpeed = 6f;
 
     // Start is called before the first frame update
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.SetPosition(0, player.position);
+
+        dist = Vector3.Distance(player.position, destination.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        lineRenderer.SetPosition(0, player.postion);
+        if(counter < dist)
+        {
+            counter += .1f / lineDrawSpeed;
+
+            float x = Mathf.Lerp(0, dist, counter);
+
+            Vector3 pointA = player.position;
+            Vector3 pointB = destination.position;
+
+            //Get the unit vector in the desired direction, multiply by the desired length and add the starting point.
+            Vector3 pointAlongLine = x * Vector3.Normalize(pointB - pointA) + pointA;
+
+            lineRenderer.SetPosition(1, pointAlongLine);
+        }
+        lineRenderer.SetPosition(0, player.position);
     }
 }
