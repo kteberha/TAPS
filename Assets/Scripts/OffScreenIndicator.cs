fd82cb3b8115efd7cb54c64ac1d7aa4a9c57043f
DESKTOP-S2Q84IH\scarl <ambeer@umail.iu.edu>
2019-10-29 14:56:48 -0400

    off screen indicators now all target the astriod homes correctly

--- Assets/Scripts/OffScreenIndicator.cs
+++ Assets/Scripts/OffScreenIndicator.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class OffScreenIndicator : MonoBehaviour
 {
-    public Vector3 target;
+    public Transform target;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +16,12 @@ public class OffScreenIndicator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         // Put it on the edge
-        Vector3 edgePt = target;
+        Vector3 edgePt = Camera.main.WorldToScreenPoint(target.position);
         edgePt.x = Mathf.Clamp(edgePt.x, Screen.width * 0.01f, Screen.width * 0.99f);
         edgePt.y = Mathf.Clamp(edgePt.y, Screen.height * 0.01f, Screen.height * 0.99f);
-        //indicators[nextIndicator].transform.position = edgePt;
+        transform.position = edgePt;
 
     }
 }
