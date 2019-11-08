79e5c473cccd8406653749a1a1014178307a90ce
KertisJones <jesse.riggins98@gmail.com>
2019-11-07 22:07:21 -0500

    Adds 10 minute Workday reset. (see GameManager on Main Camera)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,9 @@ public class GameManager : MonoBehaviour
     public int packagesDelivered = 0;
     public int points = 0;
 
+    public float timeInWorkday = 0f;
+    public float workdayLength = 600f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +20,18 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         if (Input.GetAxis("Cancel") != 0 || Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             Debug.Log("Reload scene...");
+        }
+
+        //Workday reset
+        timeInWorkday += Time.deltaTime;
+        if (timeInWorkday > workdayLength)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            Debug.Log("New Workday! Reload scene...");
         }
     }
 }
