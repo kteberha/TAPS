f15a69cf81ed406c9f533b03d27088821f6d76ba
KertisJones <jesse.riggins98@gmail.com>
2019-11-07 22:27:55 -0500

    Boxes spawn in the shipping lane every 10 seconds.

---
+++ Assets/SpawnBox.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBox : MonoBehaviour
+{
+    private float timeSinceSpawn = 0f;
+    public float spawnRate = 15f;
+
+    public GameObject box;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceSpawn += Time.deltaTime;
+        if (timeSinceSpawn >= spawnRate)
+        {
+            if (box != null)
+                Instantiate(box, new Vector3(Random.Range(-25, 25), Random.Range(-15, 30), 0f), new Quaternion());
+            timeSinceSpawn = 0;
+        }
+    }
+}
