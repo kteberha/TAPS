fad718428980733199ef4df90c5b207eb4840e92
Kaleb Eberhart <kalebeberhart@gmail.com>
2019-11-22 12:32:45 -0500

    Added Pause menu skeleton and created Quit functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,20 +15,50 @@ public class GameManager : MonoBehaviour
     public Text workdayStatusText;
     Animation textAnimation;
 
+    bool paused = false;
+    public Image pausePanel;
+    public Image resumeButton;
+    public Image optionsButton;
+    public Image quitButton;
+    Color panelColor;
+    Color resumeButtonColor;
+    Color optionsButtonColor;
+    Color quitButtonColor;
     // Start is called before the first frame update
     void Start()
     {
         workdayStatusText.text = "Clocked IN!";
         textAnimation = workdayStatusText.GetComponent<Animation>();
+        panelColor = pausePanel.color;
+        resumeButtonColor = resumeButton.color;
+        optionsButtonColor = optionsButton.color;
+        quitButtonColor = quitButton.color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxis("Cancel") != 0 || Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             Debug.Log("Reload scene...");
+        }
+
+        //pause the game on Espcape press
+        if(Input.GetAxis("Cancel") != 0 || Input.GetKeyDown(KeyCode.Escape))
+        {
+            //if (paused)
+            //{
+            //    print("resume");
+            //    Resume();
+            //}
+            //else
+            //    print("pause");
+            //    Pause();
+
+            //temporary until UI gets implemented successfully
+            UnityEditor.EditorApplication.ExecuteMenuItem("Edit/Play");
+            Application.Quit();
         }
 
         //Workday reset
@@ -50,5 +80,27 @@ public class GameManager : MonoBehaviour
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         Debug.Log("New Workday! Reload scene...");
+    }
+
+    void Pause()
+    {
+        
+        panelColor.a = 1f;
+        resumeButtonColor.a = 1f;
+        optionsButtonColor.a = 1f;
+        quitButtonColor.a = 1f;
+        Time.timeScale = 0f;
+        paused = true;
+
+    }
+
+    void Resume()
+    {
+        paused = false;
+        Time.timeScale = 1f;
+        panelColor.a = 0f;
+        resumeButtonColor.a = 0f;
+        optionsButtonColor.a = 0f;
+        quitButtonColor.a = 0f;
     }
 }
