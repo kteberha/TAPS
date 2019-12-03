7252299a62a837792317251df192ea9d4991438b
Andy Beer (laptop) <ambeerski@gmail.com>
2019-12-03 14:01:58 -0500

    zoom in button on press of m

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets._2D;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Transform player;
+    public GameObject menu;
+
+    Camera main;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        main = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("m"))
+            {
+            Debug.Log("m");
+            main.GetComponent<Camera2DFollow>().enabled = false;
+            main.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, 1.0f);
+            //menu.gameObject.SetActive(true);
+            }
+    }
+}
