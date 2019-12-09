86c95193636b2155e20120fe231c8d492d83d5a5
Kaleb Eberhart <kalebeberhart@gmail.com>
2019-12-09 11:28:31 -0500

    Minimap works perfectly now

--- Assets/MinimapCamera.cs
+++ Assets/MinimapCamera.cs
@@ -9,7 +9,7 @@ public class MinimapCamera : MonoBehaviour
     private void LateUpdate()
     {
         Vector3 newPosition = player.position;
-        newPosition.y = transform.position.y;
+        newPosition.z = transform.position.z;
         transform.position = newPosition;
     }
 }
