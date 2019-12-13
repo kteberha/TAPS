91c571098488bad938d0c19b04276a136df1bb8a
Kaleb Eberhart <kalebeberhart@gmail.com>
2019-12-13 02:35:36 -0500

    Fixed game manager bug where game wouldn't play after clock out

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -67,7 +67,7 @@ public class GameManager : MonoBehaviour
 
         yield return new WaitForSeconds(textAnimation["WorkdayStatusAnim"].length);
         
-        DontDestroyOnLoad(this);
+        //DontDestroyOnLoad(this);
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
