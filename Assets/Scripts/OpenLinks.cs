4af0227b64cd5808a20b0dc4d14867c232fa3bab
Kaleb Eberhart <kalebeberhart@gmail.com>
2020-03-11 12:22:49 -0400

    Added some files for the demo specific build, started working on splash screen stuff, creating credits screen. All of it not done but needed to transfer files

---
+++ Assets/Scripts/OpenLinks.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLinks : MonoBehaviour
+{
+    public void OpenWishList()
+    {
+        Application.OpenURL("https://store.steampowered.com/app/1244300/The_Astro_Parcel_Service/");
+    }
+
+    public void OpenTwitter()
+    {
+        Application.OpenURL("https://twitter.com/AstroParcel");
+    }
+}
