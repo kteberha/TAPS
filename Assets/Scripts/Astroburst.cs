7c5113a29426f1905193e174e4bef552f4a2ea35
Kaleb Eberhart <kalebeberhart@gmail.com>
2020-04-07 18:17:49 -0400

    Added sounds and prepped audio mixer for editing. Cleaned up some unnecessary lines of code in a handful of scripts

--- Assets/Scripts/Astroburst.cs
+++ Assets/Scripts/Astroburst.cs
@@ -5,12 +5,9 @@ using UnityEngine;
 public class Astroburst : MonoBehaviour
 {
     public GameObject blackHole;
-    public GameObject playOnCollision;
 
     PointEffector2D pointEffector;
-    ParticleSystem partSystem;
     MeshRenderer meshRend;
-    bool partIsEmitting = false;
     bool triggered = false;
 
     public float delayTime = 0.5f;
@@ -27,14 +24,19 @@ public class Astroburst : MonoBehaviour
         pointEffector = GetComponent<PointEffector2D>();
         pointEffector.enabled = false;
 
+        //make sure all the explosion particles aren't playing
         foreach (ParticleSystem _p in CollisionParticles)
             _p.Stop();
 
+        //start the continuous particle effect
         pCont.Play();
         meshRend = GetComponent<MeshRenderer>();
-
     }
 
+    /// <summary>
+    /// trigger the explosion if it is hit by a package or player
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") || other.CompareTag("Package"))
@@ -47,23 +49,27 @@ public class Astroburst : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Coroutine that handles the exploding logic
+    /// </summary>
+    /// <returns></returns>
     IEnumerator Explode()
     {
-        //trigger color change to indicate imminent detonation
-        //Color astroColor = GetComponent<MeshRenderer>().material.color;
-        //astroColor = new Color(1,0,0);
-
         //wait the desired time before detonating
         yield return new WaitForSeconds(delayTime);
 
+        //enable the point effector to force objects away
         pointEffector.enabled = true;
+        //stop the continuous particle effect
         pCont.Stop();
-        print(pCont.isPlaying);
 
+        //go through the explosion particle effects and play them
         foreach (ParticleSystem _p in CollisionParticles)
             _p.Play();
 
+        //make the star mesh disappear
         meshRend.enabled = false;
+        //play the explosion sound
         GetComponent<AudioSource>().Play();
 
         //wait for a second before disabling the collider so that the force can be applied
@@ -72,8 +78,10 @@ public class Astroburst : MonoBehaviour
         //disable colliders so force doesn't get added by accident when the mesh is gone
         GetComponent<CircleCollider2D>().enabled = false;
 
+        //wait until the particle effect is done emitting to call any more code
         yield return new WaitWhile(ParticleEmitting);
 
+        //determine if a black hole should appear or not. The first star exploding will never be a black hole
         if (!firstBurst)
         {
             float bHCreation = Random.value;
@@ -87,9 +95,14 @@ public class Astroburst : MonoBehaviour
             firstBurst = false;
         }
 
+        //destroy the astroburst asset
         Destroy(gameObject);
     }
 
+    /// <summary>
+    /// Returns true as long as the particle effect that plays the longest is emitting
+    /// </summary>
+    /// <returns></returns>
     bool ParticleEmitting()
     {
         return CollisionParticles[5].isEmitting;
