50a083844d689c231f68e5e56867c0a0b99d5272
Kaleb Eberhart <kalebeberhart@gmail.com>
2020-04-16 19:03:06 -0400

    Applied music manger to each scene

--- Assets/MusicManager.cs
+++ Assets/MusicManager.cs
@@ -8,7 +8,7 @@ public class MusicManager : MonoBehaviour
 
     GameManager gm;
     float _totalTime;
-    bool shouldLoop = false;
+    [SerializeField]bool shouldLoop = false;
 
     private void Awake()
     {
@@ -21,17 +21,23 @@ public class MusicManager : MonoBehaviour
     {
         if (loop1.isPlaying)
         {
-            //print("time samples: " + loop1.timeSamples);
-            //print("time: " + loop1.time);
-            if(loop1.time >= (loop1.clip.length - 0.2f) && !shouldLoop)
+            if (shouldLoop)
+            {
+                loop1.loop = true;
+            }
+            if (loop1.time >= (loop1.clip.length - 0.2f) && !shouldLoop)
             {
                 print("play loop 2");
                 loop2.Play();
                 loop1.Stop();
+                if (gm.workdayLength > 180)
+                    shouldLoop = true;
             }
         }
         else if(loop2.isPlaying)
         {
+            if (gm.workdayLength - gm.timeInWorkday < 50)
+                shouldLoop = false;
             if(shouldLoop)
             {
                 loop2.loop = true;
@@ -45,54 +51,4 @@ public class MusicManager : MonoBehaviour
         yield return new WaitForEndOfFrame();
         StartCoroutine(CheckForLoopChange());
     }
-
-    //IEnumerator JuggleMusic()
-    //{
-    //    //leave some time for the hype loop to repeat if day is long
-    //    if(gm.workdayLength > 151)
-    //    {
-    //        print("long day for music");
-
-    //        //start the hype section at 2 minute mark, make the loop exit at the 1 minute mark
-    //        if (gm.workdayLength - gm.timeInWorkday <= 45)
-    //            SwapClip(4);
-    //        else if (gm.workdayLength - gm.timeInWorkday <= 121)
-    //        {
-    //            SwapClip(3);
-    //            yield return new WaitForSeconds(30f);
-    //            StartCoroutine(JuggleMusic());
-    //        }
-    //        else
-    //        {
-    //            SwapClip(Random.Range(0, 3));
-    //            yield return new WaitForSeconds(30f);
-    //            StartCoroutine(JuggleMusic());
-    //        }
-
-    //        }
-    //    else
-    //    {
-    //        print("short day for music");
-    //        if(gm.workdayLength - gm.timeInWorkday > 150)
-    //        {
-    //            SwapClip(Random.Range(0, 3));
-    //            yield return new WaitForSeconds(30f);
-    //            StartCoroutine(JuggleMusic());
-    //        }
-    //        else if (gm.workdayLength - gm.timeInWorkday <= 100)
-    //        {
-    //            SwapClip(3);
-    //            StartCoroutine(JuggleMusic());
-    //        }
-    //        else if (gm.workdayLength - gm.timeInWorkday <= 45)
-    //            SwapClip(4);
-    //    }
-    //}
-
-    //void SwapClip(int clip)
-    //{
-    //    music.setParameterByName("LoopSection", clip);
-    //    print("new clip #" + clip);
-    //}
-
 }
